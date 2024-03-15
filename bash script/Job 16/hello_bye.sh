a0e8e66b5b9c7c429c65c8b8d3bdca4c547760bc
kais-fraoucene <kais.fraoucene@laplateforme.io>
2024-03-15 11:26:20 +0100

    Projet cybersécurité 'BashCeption'

---
+++ bash script/Job 16/hello_bye.sh
@@ -0,0 +1,11 @@
+a=$1
+
+if [ $a == 'hello' ]
+then
+	echo "Bonjour,je suis un script !"
+elif [ $a == 'bye' ]
+then
+	echo "Au revoir et bonne journée !"
+fi
+
+
---
+++ bash script/Job 17/my_calculator.sh
@@ -0,0 +1,14 @@
+a=$1
+b=$2
+c=$3
+
+if [ $b == "+" ]
+then
+	echo $(($a+$c))
+elif [ $b == "-" ]
+then
+	echo $(($a-$c))
+elif [ $b == "x" ]
+then
+	echo $(($a*$c))
+fi
